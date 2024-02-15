6e246f62574507a462273e4e787e1c51b5d5d526
SvintsovAnton <Svintsov.anton94@gmail.com>
2024-02-15 19:46:42 +0100

    my homework 17

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
+
 
 // 1. Компоненты пишутся с заглавной буквы
 // 2. Компоненты должны возвращать JSX(DOM элементы - HTML теги)
 function App() {
-  const nameOfCard = "Animal";
+ // const nameOfCard = "Animal";
+ // const animal = {
+ //   name: "Tiger",
+ //   age: 15,
+ //   src: "https://files.worldwildlife.org/wwfcmsprod/images/Tiger_resting_Bandhavgarh_National_Park_India/hero_small/6aofsvaglm_Medium_WW226365.jpg",
+ // }; 
 
-  const animal = {
-    name: "Tiger",
-    age: 15,
-    src: "https://files.worldwildlife.org/wwfcmsprod/images/Tiger_resting_Bandhavgarh_National_Park_India/hero_small/6aofsvaglm_Medium_WW226365.jpg",
-  };
 
   return (
-    <div className="App">
-      <p>React app lesson 17</p>
-      <div className="button-container">
-        <Button />
-        <Button />
-        <Button />
-      </div>
-      <div className="car-card">
-        <h3>{nameOfCard}</h3>
-        <img src={animal.src} alt="Tiger image" />
-        <p>Name: {animal.name}</p>
-        <p>Age: {animal.age}</p>
-      </div>
-    </div>
+<div class ="cardsInRow">
+<ProfileCard/>
+<ProfileCard/>
+<ProfileCard/>
+</div>
+    
+ //   <div className="App">
+ //     <p>React app lesson 17</p>
+   //   <div className="button-container">
+    //    <Button />
+//        <Button />
+  //      <Button />
+ //     </div>
+   //   <div className="car-card">
+     //   <h3>{nameOfCard}</h3>
+       // <img src={animal.src} alt="Tiger image" />
+       // <p>Name: {animal.name}</p>
+       // <p>Age: {animal.age}</p>
+     // </div>
+   // </div>
   );
 }
 
---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,13 @@
+function ProfileCard(){
+    return (
+        <div class="profileCard">
+            <img class = "foto_card" src="/images/forProfileCard.jpg" alt="Profile" />
+            <p class="stringInProfileCard">Name: Mike Mikle</p>
+            <p class="stringInProfileCard">Professional: actor</p>
+            <p class="stringInProfileCard">hair color and height : brown, 179 cm</p>
+            <p class="stringInProfileCard">Hobbies: traveling, mountaineering, motorcycles</p>
+        </div>
+    );
+}
+
+export default ProfileCard;
